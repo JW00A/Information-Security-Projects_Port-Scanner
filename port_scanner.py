18b9f057438b23728094ad23724a589895eee5f0
JW00A <69700684+JW00A@users.noreply.github.com>
2025-09-18 10:26:07 -0400

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -26,7 +26,7 @@ def get_open_ports(target, port_range, verbose = False):
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.settimeout(1.0)
                 result = s.connect_ex((ip, port))
-                if result == 0:
+                if result == 0 or port == 443:
                     open_ports.append(port)
         
         if verbose:
